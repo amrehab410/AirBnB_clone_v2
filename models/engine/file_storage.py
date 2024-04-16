c62773d9796936236b99f12ca716087dcf2572eb
root <root@DESKTOP-LP5R8B0.localdomain>
2024-04-16 20:51:46 +0200

    delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            records = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__:
+                    records[key] = value
+            return records
         return FileStorage.__objects
 
     def new(self, obj):
@@ -24,6 +30,12 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+    def delete(self, obj=None):
+        if obj == None:
+            return
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del(self.__objects[key])
 
     def reload(self):
         """Loads storage dictionary from file"""
