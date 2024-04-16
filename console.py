ef9a662dd44260c620ef5ff145e840a3508d1884
root <root@DESKTOP-1N3TEFC>
2024-04-16 18:35:03 +0200

    create again

--- console.py
+++ console.py
@@ -136,6 +136,7 @@ class HBNBCommand(cmd.Cmd):
                 continue
             params[param[0]] = param[1]
 
+        print("Parameters of the Instance: ",params)
         if not split_args:
             print("** class name missing **")
             return
@@ -143,6 +144,7 @@ class HBNBCommand(cmd.Cmd):
             print(args)
             print("** class doesn't exist **")
             return
+        print("Last arg:",args)
 
         new_instance = HBNBCommand.classes[className]()
         for key in params:
