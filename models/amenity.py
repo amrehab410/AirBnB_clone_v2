06cbadd1be6bc9623b95f575527a45874669eb5a
root <root@DESKTOP-1N3TEFC>
2024-04-18 03:59:50 +0200

    task 10

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,11 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
+
 
 
 class Amenity(BaseModel, Base):
     """ class Amenity inherits from BaseModel and Base """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,  back_populates="amenity")
+
--- models/place.py
+++ models/place.py
@@ -5,18 +5,6 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from models.review import Review
 from models.amenity import Amenity
 from sqlalchemy.orm import relationship
-import os
-import models
-
-place_amenity = Table(
-    "place_amenity", Base.metadata,
-    Column(
-        'place_id', String(60), ForeignKey("places.id"), primary_key=True,
-        nullable=False),
-    Column(
-        'amenity_id', String(60), ForeignKey("amenities.id"), primary_key=True,
-        nullable=False)
-)
 
 
 class Place(BaseModel, Base):
